b6bec6ae20c81eeb8f5fca42ae53e2be3ecc13df
web developer <mr.vusalmansurov@gmail.com>
2023-01-26 04:53:06 -0800

    Dosyalar yuklendi

---
+++ java.js
@@ -0,0 +1,82 @@
+/*let obj1 = {
+    name: "Mauro",
+    surname: "Icardi",
+    age: "29",
+    address: "Argentina"
+}
+
+let obj2;
+obj2 = obj1;
+
+
+console.log(obj2)
+
+*/
+
+
+
+/*let obj1 = {
+    name: "Mauro",
+    surname: "Icardi",
+    age: "29",
+
+    address: "Argentina"
+}
+
+let obj2 = {...obj1}
+
+obj2.address="Turkiye"
+
+console.log(obj1)
+console.log(obj2)
+*/
+ 
+
+/*
+let obj = {
+    name: "Dries",
+    surname: "Mertens"
+}
+
+obj.age = "23"
+
+console.log(obj)
+*/
+
+
+/*
+let obj = {
+    name: "Dries",
+    surname: "Mertens"
+}
+
+obj["age"] = "23"
+
+
+*/
+
+
+
+
+/*
+function Person(first, last, age) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+  }
+  const player = new Person("Mauro", "Icardi", 29);
+  console.log(player)
+  */
+
+
+/*const person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  console.log(person)
+*/
+
